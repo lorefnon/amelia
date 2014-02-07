e1ae7098498a98318641d53d48f9e49a55fd8abb
lorefnon <lorefnon@gmail.com>
2014-02-08 02:29:35 +0530

    Refactor out cli parameter extraction to a separate layer

--- bin/node-reader.js
+++ bin/node-reader.js
@@ -8,9 +8,60 @@ var argv = require('optimist').argv;
 var path = require('path');
 var Reader = require(path.join(__dirname, '../lib/reader' ));
 
-if ( argv.add ) { Reader.add(); }
-else if ( argv.purge ) { Reader.purge(); }
-else if ( argv.list ) { Reader.list(); }
-else if ( argv.read ) { Reader.read(); }
-else if ( argv.help ) { Reader.help(); }
-else { Reader.help(); }
+var Db = require('mongodb').Db,
+Server = require('mongodb').Server;
+
+var mongoConfig = {
+    database: 'ReaderDB',
+    host: 'localhost',
+    port: 27017
+};
+
+var db = new Db(mongoConfig.database, new Server(
+    mongoConfig.host,
+    mongoConfig.port
+), { w: 1});
+
+var controller = {
+
+    actions: {
+        add: function(reader, cb){
+            var name = process.argv[3];
+            var url = process.argv[4];
+
+            reader.add(name, url, cb);
+        },
+        purge: function(reader, cb){
+            var name = process.argv[3];
+            reader.purge(name, cb);
+        },
+        list: function(reader, cb){
+            reader.list(cb);
+        },
+        read: function(reader, cb){
+            var name = process.argv[3];
+            reader.read(name, cb);
+        }
+    },
+
+    run: function(db, cb){
+        var reader = new Reader(db);
+        for (action in this.actions) {
+            if (argv[action]) {
+                this.actions[action](reader, cb)
+                return;
+            }
+        }
+        reader.help();
+    }
+}
+
+db.open(function(err, db){
+    if (! err) {
+        controller.run(db, function(){
+            db.close();
+        });
+    } else {
+        console.dir(err);
+    }
+});
