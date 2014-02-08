43c01dd26da76f78fe44e6c64cf1da350604c7ac
lorefnon <lorefnon@gmail.com>
2014-02-08 19:40:37 +0530

    Add support for aliases and non-hyphenated options

--- bin/amelia.js
+++ bin/amelia.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-var argv = require('optimist').argv;
 var path = require('path');
 var Reader = require(path.join(__dirname, '../lib/reader' ));
 var consoleReporter = require(path.join(__dirname, '../lib/console_reporter' ));
@@ -10,6 +9,13 @@ mongoConfig = {
 }
 
 var controller = {
+
+    aliases: {
+        r: 'read', get: 'read', fetch: 'read',
+        'new': 'add',
+        del: 'purge', remove: 'purge', rm: 'purge',
+        all: 'list'
+    },
 
     actions: {
         add: function(reader){
@@ -32,32 +38,38 @@ var controller = {
         help: function(){
             console.log('Amelia - ver. 0.0.1');
             console.log('\nUsage:\n');
-            console.log('$ amelia --read "<alias>"         read RSS feed');
-            console.log('$ amelia --add "<alias>" "<url>"  add RSS feed');
-            console.log('$ amelia --purge "<alias>"        remove RSS feed');
-            console.log('$ amelia --list                   list current feeds');
+            console.log('$ amelia read "<alias>"         read RSS feed');
+            console.log('$ amelia add "<alias>" "<url>"  add RSS feed');
+            console.log('$ amelia purge "<alias>"        remove RSS feed');
+            console.log('$ amelia list                   list current feeds');
             process.exit();
         }
     },
 
+    match: function() {
+        var cmd = process.argv[2];
+        if (cmd.slice(0, 2) === '--') {
+            cmd = cmd.slice(2);
+        }
+        return this.actions[cmd] ? cmd : this.aliases[cmd];
+    },
+
     run: function(db){
         var reader = new Reader(db);
-        for (action in this.actions) {
-            if (argv[action]) {
-
-                this.actions[action](reader)
-                    .then(function(res){
-                        consoleReporter[action].success(res);
-                        process.exit();
-                    }, function(err){
-                        consoleReporter[action].error(err);
-                        process.exit();
-                    })
-                    .end();
-                return;
-            }
+        var action = this.match()
+        if (action) {
+            this.actions[action](reader)
+                .then(function(res){
+                    consoleReporter[action].success(res);
+                    process.exit();
+                }, function(err){
+                    consoleReporter[action].error(err);
+                    process.exit();
+                })
+                .end();
+        } else {
+            this.actions.help();
         }
-        this.actions.help();
     }
 }
 
